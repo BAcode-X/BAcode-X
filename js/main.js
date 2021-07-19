4ea8fef0bb1fae3bb732da103c89449fa79470b2
Biruk Alamirew <biruk.alamirew77@gmail.com>
2021-07-19 13:44:55 +0300

    created the basic structure and nav links

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var cards = document.getElementsByClassName('card-btn');
+
+
+// cards.forEach(el => {
+//     el.addEventListener('mouseenter', e => {
+//         e.target.style.color = 'white';
+//     })
+// });

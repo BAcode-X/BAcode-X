d400df9574a127aa75b474695027b090557ac533
Biruk Alamirew <biruk.alamirew77@gmail.com>
2021-07-23 01:19:04 +0300

    added the card sliding animation

--- js/main.js
+++ js/main.js
@@ -1,8 +1,41 @@
-var cards = document.getElementsByClassName('card-btn');
+const cards = document.querySelectorAll('.card-btn');
+const home = document.querySelector('#home')
+const about = document.querySelector('#about')
 
 
-// cards.forEach(el => {
-//     el.addEventListener('mouseenter', e => {
-//         e.target.style.color = 'white';
-//     })
-// });
+cards[0].classList.add('active')
+
+
+// add active class
+const addActiveClass = () => {
+        cards.forEach(card => {
+            card.addEventListener('click', () => {
+                removeClass();
+                card.classList.add('active')
+            })
+        })
+    }
+    // call the function
+addActiveClass();
+
+
+const removeClass = () => {
+    cards.forEach(card => {
+        card.classList.remove('active')
+    })
+}
+
+window.addEventListener('scroll', () => {
+    const triggerTop = window.innerHeight / 5 * 2
+
+    const aboutTop = about.getBoundingClientRect().top
+    const homeTop = home.getBoundingClientRect().top
+
+
+    if (aboutTop < triggerTop) {
+        about.classList.add('active')
+    } else {
+        about.classList.remove('active')
+    }
+
+})
